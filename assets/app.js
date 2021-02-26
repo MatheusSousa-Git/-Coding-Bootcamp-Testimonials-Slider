b3fb0ee4280fc82beb2afaa6be89d3b70dd5a72b
MatheusSousa <73663610+MatheusSousa-Git@users.noreply.github.com>
2021-02-25 22:22:40 -0300

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,30 @@
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const slides = document.querySelectorAll('.slide');
+
+let index = 0;
+
+function display () {
+    slides.forEach((slide) => {
+    slide.style.display = 'none';
+    });
+    slides[index].style.display = 'flex';
+}
+
+function nextSlide () {
+    index++;
+    if (index > slides.length - 1) {
+        index = 0;
+    }
+   display ();
+}
+function prevSlide () {
+    index--;
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+    display ();
+}
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
